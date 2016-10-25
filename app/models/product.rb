973ab37ac8a0c842a8f907a79828eb226507b6ec
Loi Tran <lvt15@my.fsu.edu>
2016-10-25 21:06:44 +0700

    added the cost test

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,15 @@ class Product < ActiveRecord::Base
   def self.alphabetical
     order("name")
   end
+
+  def discount_amount
+    if price_vnd > 100000
+      price_vnd = self.price_vnd * 0.79
+    elsif price_vnd > 200000
+      price_vnd = self.price_vnd * 0.69
+    else price_vnd > 800000
+      price_vnd = self.price_vnd * 0.59
+    end
+  end
+
 end
--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,7 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe Product, type: :model do
-<<<<<<< HEAD
   pending "add some examples to (or delete) #{__FILE__}"
 
   describe ".alphabetical" do
@@ -19,24 +18,21 @@ RSpec.describe Product, type: :model do
       b = Product.create! name: "b"
       expect(Product.alphabetical).to eq [a,b]
     end
-
-=======
-  #pending "add some examples to (or delete) #{__FILE__}"
-  describe ".alphabetical" do
-    it "returns [] if there are no product" do
-      expect(Product.alphabetical).to eq [] 
-    end
-
-    it "returns [a] if there are 1 product a" do
-      a = Product.create name: "a"
-      expect(Product.alphabetical).to eq [a] 
-    end
-
-    it "returns [a,b] if there are 2 product a, b and a before b" do
-      a = Product.create name: "a"
-      b = Product.create name: "b"
-      expect(Product.alphabetical).to eq [a,b] 
-    end
->>>>>>> 30404d1aa79cc10cd458dc0cab8b1ea571c4eecc
   end
+
+  describe ".discount_amount" do
+    it "applies discount to the price" do
+      a = Product.create! price_vnd: 150000
+      expect(a.discount_amount).to eq 118500
+    end
+    # it "" do
+    #
+    # end
+    # it "" do
+    #
+
+    # end
+  end
+
+
 end
