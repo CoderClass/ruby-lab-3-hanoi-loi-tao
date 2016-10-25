30404d1aa79cc10cd458dc0cab8b1ea571c4eecc
Tao Nhu <tao.nhu@sugar.sg>
2016-10-25 20:49:29 +0700

    milestone 5

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe Product, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  #pending "add some examples to (or delete) #{__FILE__}"
+  describe ".alphabetical" do
+    it "returns [] if there are no product" do
+      expect(Product.alphabetical).to eq [] 
+    end
+
+    it "returns [a] if there are 1 product a" do
+      a = Product.create name: "a"
+      expect(Product.alphabetical).to eq [a] 
+    end
+
+    it "returns [a,b] if there are 2 product a, b and a before b" do
+      a = Product.create name: "a"
+      b = Product.create name: "b"
+      expect(Product.alphabetical).to eq [a,b] 
+    end
+  end
 end
